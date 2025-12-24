233f94b69debd4f476253d2e4a995bbe7c8d300a
Achira Menath <achiramenath92@gmail.com>
2025-12-24 14:33:40 +0530

    Changes

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import SearchDonor from "./Pages/Search-doner/Search-doner";
 import DonateBlood from "./Pages/Donate/Donate-blood";
 import AboutUs from "./Pages/Feedback/Aboutus";
 import Contact from "./Pages/Feedback/Contactus";
-import Viewprofile from "./Pages/Profile/View-profile"
+import Viewprofile from "./Pages/Profile/View-profile";
 import Editprofile from "./Pages/Profile/Edit-profile";
 import Beforelogin from "./Pages/Home-login/Home-page";
 import Login from "./Pages/Home-login/Login";
@@ -17,10 +17,9 @@ import Feedbackthanks from "./Pages/Feedback/feedback-thank";
 import Donatebloodconfirm from "./Pages/Donate/Donateconfirm";
 import Signupthank from "./Pages/Home-login/Thank-for-register";
 
-
 function App() {
   return (
-    <BrowserRouter basename="/blood-donation-management-system">
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<Beforehome />} />
         <Route path="/home" element={<Home />} />
@@ -28,18 +27,18 @@ function App() {
         <Route path="/donate" element={<DonateBlood />} />
         <Route path="/about" element={<AboutUs />} />
         <Route path="/contact" element={<Contact />} />
-        <Route path="/Viewprofile" element={<Viewprofile />} />
-        <Route path="/Editprofile" element={<Editprofile />} />
-        <Route path="/Beforelogin" element={<Beforelogin />} />
-        <Route path="/Login" element={<Login />} />
-        <Route path="/Signup" element={<Signup />} />
-        <Route path="/Feedback" element={<Feedback />} />
-        <Route path="/Feedbackthanks" element={<Feedbackthanks />} />
-        <Route path="/Donateconfirm" element={<Donatebloodconfirm />} />
+        <Route path="/viewprofile" element={<Viewprofile />} />
+        <Route path="/editprofile" element={<Editprofile />} />
+        <Route path="/beforelogin" element={<Beforelogin />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/feedback" element={<Feedback />} />
+        <Route path="/feedbackthanks" element={<Feedbackthanks />} />
+        <Route path="/donateconfirm" element={<Donatebloodconfirm />} />
         <Route path="/signupthank" element={<Signupthank />} />
-
       </Routes>
     </BrowserRouter>
   );
 }
+
 export default App;
--- src/components/Headerblank.jsx
+++ src/components/Headerblank.jsx
@@ -1,5 +1,3 @@
-import React from 'react';
-import { Link } from "react-router-dom";
 import './Headerblank.css'; 
 import Hedimg from "../assets/BLD.png";
 
